68b56bcb9b2f6ca1ecc9bdccaa10c670a89549e6
Joey Perrott <jperrott@google.com>
2015-09-22 13:50:13 -0700

    add bottom sheet and menu button in frame

--- shrine/app/src/store/CatalogController.js
+++ shrine/app/src/store/CatalogController.js
@@ -2,50 +2,14 @@
  * Main App Controller for the Angular Material Starter App
  */
 class CatalogController {
-  constructor($mdSidenav, $log, itemsService) {
+  constructor($log, itemsService) {
     this.$log = $log.getInstance("CatalogController");
     this.$log.debug("instanceOf()");
     this.$log.debug(itemsService);
 
     this.items = itemsService.items;
-    this.$mdSidenav = $mdSidenav;
-    console.log(itemsService.items)
-
-    this.categories = [
-      { title: 'Feature'},
-      { title: 'Latest'},
-      { title: 'Fashion'},
-      { title: 'Furniture'},
-      { title: 'Beauty'},
-      { title: 'Food'},
-      { title: 'Travel'},
-      { title: 'Kids'}
-    ];
-  }
-
-  /**
-   * Hide or Show the category menu.
-   */
-  toggleMenu($event) {
-    this.$log.debug( "toggleMenu() ");
-    this.$mdSidenav('left').toggle();
-  }
-
-  /**
-   * Select a category.
-   * @param index Index of the selected category.
-   */
-  selectCategory(index) {
-    this.$log.debug( "selectCategory({})", index);
-
-    this.selectedIdx = index;
-    this.$mdSidenav('left').close()
-  }
-
-  currentCategory() {
-    return this.categories[this.selectedIdx];
   }
 }
 
-CatalogController.$inject = [ '$mdSidenav', '$log', 'ItemsService' ];
+CatalogController.$inject = [ '$log', 'ItemsService' ];
 export default CatalogController;
---
+++ shrine/app/src/store/SharingService.js
@@ -0,0 +1,24 @@
+// Simple service holding static item data.
+class SharingService {
+  constructor() {
+    this.sharingOptions = [
+      
+      { label: "Tweet Recipe",
+        icon: "twitter",
+      },
+      { label: "Email Recipe",
+        icon: "email",
+      },
+      { label: "Message Recipe",
+        icon: "message",
+      },
+      { label: "Share on Facebook",
+        icon: "facebook",
+      },
+    ]
+  }
+}
+
+SharingService.$inject = [];
+export default SharingService;
+
