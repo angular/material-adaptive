27887515201db48e633174eb1d4e26e7ce0bfeba
Michael Giambalvo <heathkit@google.com>
2015-09-23 21:55:49 -0700

    More cats.

--- shrine/app/src/store/ItemsService.js
+++ shrine/app/src/store/ItemsService.js
@@ -4,14 +4,23 @@ class ItemsService {
     let LOREM = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus mauris dolor, euismod ut libero et, suscipit porttitor lacus. Donec vitae consequat mauris. Proin metus lacus, euismod scelerisque ultricies in, porttitor quis ex. Suspendisse tempus eget tortor in ultricies."
     this.items = [
       
-      { name: "A cat",
+      { name: "Meowrio Lopez",
         img: "cat1.jpg",
         description: LOREM },
-      { name: "Another cat",
+      { name: "Fuzz Aldrin",
         img: "cat2.jpg",
         description: LOREM },
-      { name: "A different cat",
+      { name: "Cat Benetar",
         img: "cat3.jpg",
+        description: LOREM },
+      { name: "Chairman Meow",
+        img: "cat4.jpg",
+        description: LOREM },
+      { name: "Fleyonce Knowles",
+        img: "cat5.jpg",
+        description: LOREM },
+      { name: "George Benard Paw",
+        img: "cat6.jpg",
         description: LOREM },
     ]
   }
