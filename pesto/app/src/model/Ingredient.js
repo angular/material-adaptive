7eb3484372ee1ebe41bc8d9630cabea041903e6a
Brian Nenninger <bnenning@google.com>
2015-10-04 16:37:04 -0400

    Somewhat functional animation when selecting recipe.

--- pesto/app/src/model/Ingredient.js
+++ pesto/app/src/model/Ingredient.js
@@ -1,6 +1,5 @@
 class Ingredient {
   constructor(params) {
-    console.log('Ingredient: ' + JSON.stringify(params));
     if (!params) params = {};
     this.amount = params['amount'] || 0;
     this.unit = params['unit'] || '';
