04abb527fc2078bdf8eec81cdd002b1383425e11
josephperrott <josephperrott@gmail.com>
2015-10-20 23:15:48 +0100

    Add new images, remove old images

--- shrine/app/src/store/ItemsService.js
+++ shrine/app/src/store/ItemsService.js
@@ -12,10 +12,6 @@ class ItemsService {
         url: 'furniture',
       },
       {
-        name: 'Beauty',
-        url: 'beauty',
-      },
-      {
         name: 'Fashion',
         url: 'fashion',
       },
@@ -24,61 +20,25 @@ class ItemsService {
         url: 'food',
       },
       {
-        name: 'Travel',
-        url: 'travel',
-      },
-      {
         name: 'Kids',
         url: 'kids'
+      },
+      {
+        name: 'Outdoors',
+        url: 'outdoors',
+      },
+      {
+        name: 'Tools',
+        url: 'tools',
       }
     ];
     this.items = [
-      {
-        category: 'travel',
-        url: 'kayaks',
-        img: 'assets/images/kayaks.jpg',
-        name: 'Kayaks',
-        description: 'These are Kayaks.',
-        featured: false
-      },
-      {
-        category: 'fashion',
-        url: 'high_tops',
-        img: 'assets/images/high_tops.jpg',
-        name: 'Red High Top Shoes',
-        description: 'These are red shoes.',
-        featured: false
-      },
-      {
-        category: 'travel',
-        url: 'fishing',
-        img: 'assets/images/fishing.jpg',
-        name: 'Fishing',
-        description: 'This is fishing',
-        featured: true
-      },
-      {
-        category: 'travel',
-        url: 'beach',
-        img: 'assets/images/beach.jpg',
-        name: 'Beach',
-        description: 'This is what a beach looks like',
-        featured: false
-      },
       {
         category: 'furniture',
         url: 'green_chair',
         img: 'assets/images/green_chair.jpg',
         name: 'Green Chair',
         description: 'This is a green chair',
-        featured: false
-      },
-      {
-        category: 'travel',
-        url: 'snowboarding',
-        img: 'assets/images/snowboarding.jpg',
-        name: 'Snowboarding',
-        description: 'This is snowboarding',
         featured: false
       },
       {
@@ -90,51 +50,11 @@ class ItemsService {
         featured: true
       },
       {
-        category: 'furniture',
-        url: 'welding',
-        img: 'assets/images/welding.jpg',
-        name: 'Welding',
-        description: 'This is welding',
-        featured: false
-      },
-      {
-        category: 'kids',
-        url: 'horse',
-        img: 'assets/images/horse.jpg',
-        name: 'Horse',
-        description: 'This is a horse',
-        featured: false
-      },
-      {
-        category: 'kids',
-        url: 'cow',
-        img: 'assets/images/cow.jpg',
-        name: 'Cow',
-        description: 'This is a cow',
-        featured: false
-      },
-      {
-        category: 'beauty',
+        category: 'fashion',
         url: 'lipstick',
         img: 'assets/images/lipstick.jpg',
         name: 'Lipstick',
         description: 'These are tubes of lipstick.',
-        featured: true
-      },
-      {
-        category: 'travel',
-        url: 'skiing',
-        img: 'assets/images/skiing.jpg',
-        name: 'Skiing',
-        description: 'This is skiing',
-        featured: false
-      },
-      {
-        category: 'furniture',
-        url: 'typewriter',
-        img: 'assets/images/typewriter.jpg',
-        name: 'Typewriter',
-        description: 'This is a typewriter',
         featured: false
       },
       {
@@ -144,6 +64,166 @@ class ItemsService {
         name: 'Popsicle',
         description: 'This is a red popsicle',
         featured: true
+      },
+      {
+        category: 'kids',
+        url: 'backpack',
+        img: 'assets/images/backpack.jpg',
+        name: 'Backpack',
+        description: 'This is a backpack',
+        featured: true
+      },
+      {
+        category: 'kids',
+        url: 'beach_ball',
+        img: 'assets/images/beach_ball.jpg',
+        name: 'Beach Ball',
+        description: 'This is a beach ball',
+        featured: false
+      },
+      {
+        category: 'outdoors',
+        url: 'binoculars',
+        img: 'assets/images/binoculars.jpg',
+        name: 'Binoculars',
+        description: 'This is a pair of binoculars',
+        featured: false
+      },
+      {
+        category: 'furniture',
+        url: 'clock',
+        img: 'assets/images/clock.jpg',
+        name: 'Clock',
+        description: 'This is a clock',
+        featured: false
+      },
+      {
+        category: 'outdoors',
+        url: 'flippers',
+        img: 'assets/images/flippers.jpg',
+        name: 'Flippers',
+        description: 'This is a pair of flippers',
+        featured: false
+      },
+      {
+        category: 'fashion',
+        url: 'green_shoes',
+        img: 'assets/images/green_shoes.jpg',
+        name: 'Green Shoes',
+        description: 'This is a pair of green shoes',
+        featured: false
+      },
+      {
+        category: 'outdoors',
+        url: 'helmet',
+        img: 'assets/images/helmet.jpg',
+        name: 'Helmet',
+        description: 'This is a helmet.',
+        featured: true
+      },
+      {
+        category: 'furniture',
+        url: 'lamp',
+        img: 'assets/images/lamp.jpg',
+        name: 'Lamp',
+        description: 'This is a lamp',
+        featured: false
+      },
+      {
+        category: 'furniture',
+        url: 'lawn_chair',
+        img: 'assets/images/lawn_chair.jpg',
+        name: 'Lawn Chair',
+        description: 'This is a lawn chair',
+        featured: false
+      },
+      {
+        category: 'tools',
+        url: 'paintbrush',
+        img: 'assets/images/paintbrush.jpg',
+        name: 'Paintbrush',
+        description: 'This is a paintbrush',
+        featured: false
+      },
+      {
+        category: 'tools',
+        url: 'pen',
+        img: 'assets/images/pen.jpg',
+        name: 'Pen',
+        description: 'This is a pen',
+        featured: false
+      },
+      {
+        category: 'furniture',
+        url: 'radio',
+        img: 'assets/images/radio.jpg',
+        name: 'Radio',
+        description: 'This is a radio',
+        featured: false
+      },
+      {
+        category: 'furniture',
+        url: 'red_typewriter',
+        img: 'assets/images/red_typewriter.jpg',
+        name: 'Red Typewriter',
+        description: 'This is a red typewriter',
+        featured: false
+      },
+      {
+        category: 'fashion',
+        url: 'shoes',
+        img: 'assets/images/shoes.jpg',
+        name: 'Shoes',
+        description: 'This is a pair of shoes',
+        featured: false
+      },
+      {
+        category: 'fashion',
+        url: 'sunglasses',
+        img: 'assets/images/sunglasses.jpg',
+        name: 'sunglasses',
+        description: 'This is a pair of sunglasses',
+        featured: false
+      },
+      {
+        category: 'outdoors',
+        url: 'surfboard',
+        img: 'assets/images/surfboard.jpg',
+        name: 'Surfboard',
+        description: 'This is a surfboard',
+        featured: false
+      },
+      {
+        category: 'food',
+        url: 'teapot',
+        img: 'assets/images/teapot.jpg',
+        name: 'Teapot',
+        description: 'This is a teapot',
+        featured: false
+      },
+      {
+        category: 'outdoors',
+        url: 'tent',
+        img: 'assets/images/tent.jpg',
+        name: 'Tent',
+        description: 'This is a tent',
+        featured: false
+      },
+      {
+        category: 'tools',
+        url: 'tools',
+        img: 'assets/images/tools.jpg',
+        name: 'Tools',
+        description: 'This is a set of tools',
+        featured: false
+      },
+      {
+        category: 'fashion',
+        url: 'umbrella',
+        img: 'assets/images/umbrella.jpg',
+        name: 'Umbrella',
+        description: 'This is an umbrella',
+        featured: false
       }
     ];
   };
