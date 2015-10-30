919c476965ecafe9393621c546ea70d352932ba2
Thomas Burleson <ThomasBurleson@Gmail.com>
2015-10-29 22:27:16 -0500

    update(config): use AngularJS 1.4.7

--- shrine/app/config.js
+++ shrine/app/config.js
@@ -10,35 +10,35 @@ System.config({
   },
 
   map: {
-    "angular": "github:angular/bower-angular@1.4.6",
-    "angular-animate": "github:angular/bower-angular-animate@1.4.6",
-    "angular-aria": "github:angular/bower-angular-aria@1.4.6",
+    "angular": "github:angular/bower-angular@1.4.7",
+    "angular-animate": "github:angular/bower-angular-animate@1.4.7",
+    "angular-aria": "github:angular/bower-angular-aria@1.4.7",
     "angular-material": "github:angular/bower-material@master",
     "angular-route": "github:angular/bower-angular-route@1.4.6",
     "angular-ui-router": "github:angular-ui/ui-router@0.2.15",
-    "css": "github:systemjs/plugin-css@0.1.17",
+    "css": "github:systemjs/plugin-css@0.1.18",
     "json": "github:systemjs/plugin-json@0.1.0",
     "text": "github:systemjs/plugin-text@0.0.2",
-    "traceur": "github:jmcriffey/bower-traceur@0.0.90",
-    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.90",
+    "traceur": "github:jmcriffey/bower-traceur@0.0.91",
+    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.91",
     "ui-router": "npm:ui-router@0.0.1",
     "github:angular-ui/ui-router@0.2.15": {
-      "angular": "github:angular/bower-angular@1.4.6"
+      "angular": "github:angular/bower-angular@1.4.7"
     },
-    "github:angular/bower-angular-animate@1.4.6": {
-      "angular": "github:angular/bower-angular@1.4.6"
+    "github:angular/bower-angular-animate@1.4.7": {
+      "angular": "github:angular/bower-angular@1.4.7"
     },
-    "github:angular/bower-angular-aria@1.4.6": {
-      "angular": "github:angular/bower-angular@1.4.6"
+    "github:angular/bower-angular-aria@1.4.7": {
+      "angular": "github:angular/bower-angular@1.4.7"
     },
     "github:angular/bower-angular-route@1.4.6": {
-      "angular": "github:angular/bower-angular@1.4.6"
+      "angular": "github:angular/bower-angular@1.4.7"
     },
     "github:angular/bower-material@master": {
-      "angular": "github:angular/bower-angular@1.4.6",
-      "angular-animate": "github:angular/bower-angular-animate@1.4.6",
-      "angular-aria": "github:angular/bower-angular-aria@1.4.6",
-      "css": "github:systemjs/plugin-css@0.1.17"
+      "angular": "github:angular/bower-angular@1.4.7",
+      "angular-animate": "github:angular/bower-angular-animate@1.4.7",
+      "angular-aria": "github:angular/bower-angular-aria@1.4.7",
+      "css": "github:systemjs/plugin-css@0.1.18"
     },
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
