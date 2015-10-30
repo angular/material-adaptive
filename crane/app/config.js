1fd400526e48ebe83e8650b49d971a8e8ea8a583
Thomas Burleson <ThomasBurleson@Gmail.com>
2015-10-29 22:27:16 -0500

    update(config): use AngularJS 1.4.7

--- crane/app/config.js
+++ crane/app/config.js
@@ -12,30 +12,30 @@ System.config({
   },
 
   map: {
-    "angular": "github:angular/bower-angular@1.4.1",
-    "angular-animate": "github:angular/bower-angular-animate@1.4.1",
-    "angular-aria": "github:angular/bower-angular-aria@1.4.1",
+    "angular": "github:angular/bower-angular@1.4.7",
+    "angular-animate": "github:angular/bower-angular-animate@1.4.7",
+    "angular-aria": "github:angular/bower-angular-aria@1.4.7",
     "angular-material": "github:angular/bower-material@master",
-    "angular-route": "github:angular/bower-angular-route@1.4.1",
-    "css": "github:systemjs/plugin-css@0.1.13",
+    "angular-route": "github:angular/bower-angular-route@1.4.7",
+    "css": "github:systemjs/plugin-css@0.1.18",
     "json": "github:systemjs/plugin-json@0.1.0",
     "text": "github:systemjs/plugin-text@0.0.2",
     "traceur": "github:jmcriffey/bower-traceur@0.0.90",
     "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.90",
-    "github:angular/bower-angular-animate@1.4.1": {
-      "angular": "github:angular/bower-angular@1.4.1"
+    "github:angular/bower-angular-animate@1.4.7": {
+      "angular": "github:angular/bower-angular@1.4.7"
     },
-    "github:angular/bower-angular-aria@1.4.1": {
-      "angular": "github:angular/bower-angular@1.4.1"
+    "github:angular/bower-angular-aria@1.4.7": {
+      "angular": "github:angular/bower-angular@1.4.7"
     },
-    "github:angular/bower-angular-route@1.4.1": {
-      "angular": "github:angular/bower-angular@1.4.1"
+    "github:angular/bower-angular-route@1.4.7": {
+      "angular": "github:angular/bower-angular@1.4.7"
     },
     "github:angular/bower-material@master": {
-      "angular": "github:angular/bower-angular@1.4.1",
-      "angular-animate": "github:angular/bower-angular-animate@1.4.1",
-      "angular-aria": "github:angular/bower-angular-aria@1.4.1",
-      "css": "github:systemjs/plugin-css@0.1.13"
+      "angular": "github:angular/bower-angular@1.4.7",
+      "angular-animate": "github:angular/bower-angular-animate@1.4.7",
+      "angular-aria": "github:angular/bower-angular-aria@1.4.7",
+      "css": "github:systemjs/plugin-css@0.1.18"
     }
   }
 });
