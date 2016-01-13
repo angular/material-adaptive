315ed8cfa4adf78633c471c6c2af385e9818e5a1
Cristobal Chao Gersol <cristobalchao@cristobalchao-macbookpro.roam.corp.google.com>
2016-01-12 18:48:58 -0800

    Deleted previous version: Shrine

--- _shrine/app/src/utils/ShrineUtils.js
+++
@@ -1,9 +0,0 @@
-import ShrineDomUtils from 'utils/ShrineDomUtils';
-import constantsModuleName from 'utils/ShrineConstants';
-
-let moduleName = angular.module('shrine.utils', [constantsModuleName])
-    .service('ShrineDomUtils', ShrineDomUtils)
-    .name;
-
-export default moduleName;
-    
